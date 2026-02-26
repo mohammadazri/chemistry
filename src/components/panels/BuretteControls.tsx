61e2f1645bf075d0ceae419d23ae19fa53376b35
Mohammad Azri <mohamedazri655@gmail.com>
2026-02-26 22:52:09 +0800

    feat: Create Right Sidebar Layout

---
+++ src/components/panels/BuretteControls.tsx
@@ -0,0 +1,3 @@
+export default function BuretteControls() {
+    return <div>BuretteControls Component Placeholder</div>;
+}
---
+++ src/components/panels/DataTable.tsx
@@ -0,0 +1,3 @@
+export default function DataTable() {
+    return <div>DataTable Component Placeholder</div>;
+}
