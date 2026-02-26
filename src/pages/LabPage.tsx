00acb3f261aef6e8d0819f677765ffecfda6e914
Mohammad Azri <mohamedazri655@gmail.com>
2026-02-26 23:23:15 +0800

    feat: Add Results Modal

--- src/pages/LabPage.tsx
+++ src/pages/LabPage.tsx
@@ -2,12 +2,14 @@ import LabScene from '../components/lab/LabScene';
 import RightSidebar from '../components/panels/RightSidebar';
 import TutorialOverlay from '../components/tutorial/TutorialOverlay';
 import QuizModal from '../components/tutorial/QuizModal';
+import ResultsModal from '../components/results/ResultsModal';
 
 export default function LabPage() {
     return (
         <div className="flex w-full h-screen overflow-hidden">
             <TutorialOverlay />
             <QuizModal />
+            <ResultsModal />
             <LabScene />
             <RightSidebar />
         </div>
